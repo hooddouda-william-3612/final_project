84b8415ce717fdc94c5a97ee31de0a4b895f2c48
hooddouda-william-3612 <49251724+hooddouda-william-3612@users.noreply.github.com>
2019-06-07 11:42:47 -0700

    backslashes

--- test_final.py
+++ test_final.py
@@ -6,7 +6,7 @@ import pytest
 def test_write_instance(input_1, expected):
     assert isinstance(input_1, expected)
 
-@pytest.mark.parametrize("expected", [getcwd() + "\\downloads\\"])
+@pytest.mark.parametrize("expected", [getcwd() + "/downloads/"])
 def test_delete(expected):
     test_write = WriteNote("test_jk.txt")
     test_write.get_pictures()
@@ -14,7 +14,7 @@ def test_delete(expected):
     delete()
     assert path.isdir(expected) == False
 
-@pytest.mark.parametrize("input_1, expected", [("test_jk.txt", getcwd() + "\\downloads\\j"), ("test_jk.txt", getcwd() + "\\downloads\\k")])
+@pytest.mark.parametrize("input_1, expected", [("test_jk.txt", getcwd() + "/downloads/j"), ("test_jk.txt", getcwd() + "/downloads/k")])
 def test_make_folders(input_1, expected):
     test = WriteNote(input_1)
     test.get_pictures()
